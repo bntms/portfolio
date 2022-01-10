07be0252405b5ee8c1f3ff4e56a72d7b568605cb
Viktor Borszeki <vborszeki@gmail.com>
2022-01-10 20:50:48 +0100

    update studiob link

--- src/links.js
+++ src/links.js
@@ -2,7 +2,7 @@ export const links = {
   instagram: 'https://www.instagram.com/benetamas_works/',
   viztorony: 'https://www.facebook.com/viztoronyobuda/',
   palma: 'http://www.palmaban.hu/',
-  studiob: 'http://studiob.mome.hu',
+  studiob: 'https://www.instagram.com/mome_studiob/',
   tel: '0036706338750',
   telHref: '+36-70-633-8750',
   mail: 'info@benetamas.com',
